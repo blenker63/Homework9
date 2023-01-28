32fe0980dc5a54790360803d5e5cda65bd1c5a90
vaceslavkameronov <slka@mail.ru>
2023-01-29 03:33:14 +0400

    Решение Задачи 4

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,7 @@ public class Main {
         task1();
         task2();
         task3();
-        //task4();
-        //task5();
-        //task6();
-        //task7();
-        //task8();
+        task4();
     }
 
     // Объявить метод «сгенерироватьМассив»
@@ -41,25 +37,36 @@ public class Main {
         int minSpentDay = 1_000_000;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxSpentDay) {
-                maxSpentDay = arr[i]; }
-                {
-                    if (arr[i] < minSpentDay) {
-                    minSpentDay = arr[i]; }
-                    }
+                maxSpentDay = arr[i];
+            }
+            {
+                if (arr[i] < minSpentDay) {
+                    minSpentDay = arr[i];
+                }
+            }
         }
         System.out.println("Минимальная сумма трат за день составила " + minSpentDay + " рублей. Максимальная сумма трат за день составила " + maxSpentDay + " рублей.");
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         int[] arr = generateRandomArray(); // объявление массива и вызов метода сгенерироватьМассив
         //System.out.println(Arrays.toString(arr)); //проверка сгенерированного массива
         float amountSpentAverage = 0;
         for (int i = 0; i < arr.length; i++) {
-            amountSpentAverage = (amountSpentAverage + arr[i]);}
+            amountSpentAverage = (amountSpentAverage + arr[i]);
+        }
         System.out.println("Средняя сумма трат за месяц составила " + amountSpentAverage / arr.length + " рублей.");
-
     }
 
+    public static void task4() {
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n','a','v','I',' ','v','o','n','a','v','I'};
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[reverseFullName.length - 1 - i]);
+        }
+        }
     }
+
 
 
