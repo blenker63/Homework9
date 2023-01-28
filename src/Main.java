4cad79547ae242e58bec8d7bf69f8db1ffed1a46
vaceslavkameronov <slka@mail.ru>
2023-01-29 02:01:38 +0400

    Решение Задач 1 и 2

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+    }
+
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray(); // объявление массива и вызов метода сгенерироватьМассив
+        //System.out.println(Arrays.toString(arr)); прповерка сгенерированного массива
+        int amountSpentMonth = 0; // инициализация суммы трат за месяц
+        for (int i = 0; i < arr.length; i++) {
+            amountSpentMonth = amountSpentMonth + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + amountSpentMonth + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray(); // объявление массива и вызов метода сгенерироватьМассив
+        System.out.println(Arrays.toString(arr)); //проверка сгенерированного массива
+        int maxSpentDay = 0;
+        int minSpentDay = 1_000_000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSpentDay) {
+                maxSpentDay = arr[i]; }
+                {
+                    if (arr[i] < minSpentDay) {
+                    minSpentDay = arr[i]; }
+                    }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minSpentDay + " рублей. Максимальная сумма трат за день составила " + maxSpentDay + " рублей.");
+    }
+}
